d02757966b8e65e94f62793c0d948e31c5adc1d0
Daniel Tedesco <dtedesco1@gmail.com>
2023-07-04 17:03:45 +0800

    init 3

---
+++ selfheal-py-3/functions.py
@@ -0,0 +1,10 @@
+functionParams = {
+"description": "Describe what this object does so you can understand it in the future.",
+"path": "Location of the object, so other files can import it as needed.",
+"name": "name of the object, so it can be called.",
+"args": "typed list of arguments for the object (if any)",
+"returns": "typed list of what the object returns (if anything)"
+}
+
+
+prompt = "Write a summary of functions in this program, so that you can refer back to them over time."
